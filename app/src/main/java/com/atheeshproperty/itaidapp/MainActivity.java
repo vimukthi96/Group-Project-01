5ea5cd0c177ba9cc7a08fc2186b7ff89ace6c9cf
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2018-09-05 16:39:10 +0530

    added side drawer

--- app/src/main/java/com/atheeshproperty/itaidapp/MainActivity.java
+++ app/src/main/java/com/atheeshproperty/itaidapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.atheeshproperty.itaidapp;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,21 +13,39 @@ import com.atheeshproperty.itaidapp.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Button btnNavToProgrammingScreen =  findViewById(R.id.btnProgramming);
         Button btnNavToWeb = findViewById(R.id.btnWebDevelopment);
         Button btnNavToStatistics = findViewById(R.id.btnStatistics);
         Button btnNavToMathematics = findViewById(R.id.btnMathematics);
 
-
         btnNavToProgrammingScreen.setOnClickListener(this);
         btnNavToWeb.setOnClickListener(this);
         btnNavToStatistics.setOnClickListener(this);
         btnNavToMathematics.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
